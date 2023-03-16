c15342f5066689b9fbfe1a539f81ed7e4cdd4cad
xiongkb <xiong.kang.b@gmail.com>
2023-03-15 23:48:16 -0700

    font gameover

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public string loadScene;
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player"){
+            // go to gameover screen menu
+            SceneManager.LoadScene(loadScene);
+        }
+    }
+}
