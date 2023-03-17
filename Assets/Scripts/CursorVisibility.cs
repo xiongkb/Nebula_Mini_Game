006a54ba4eb444d5591a0aaa74460d788fa46231
xiongkb <xiong.kang.b@gmail.com>
2023-03-16 22:25:16 -0700

    level 3 + 2

--- Assets/Scripts/CursorVisibility.cs
+++ Assets/Scripts/CursorVisibility.cs
@@ -7,12 +7,20 @@ public class CursorVisibility : MonoBehaviour
 {
     void Start(){
         Scene scene = SceneManager.GetActiveScene();
-        if(scene.name == "Level_01"){
-           Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false; 
-        } else {
+        // if(scene.name == "Level_01"){
+        //    Cursor.lockState = CursorLockMode.Locked;
+        //     Cursor.visible = false; 
+        // } else {
+        //    Cursor.lockState = CursorLockMode.Confined;
+        //     Cursor.visible = true; 
+        // }
+
+        if(scene.name == "Credits" || scene.name == "Game_Over" || scene.name == "Start_Menu"){
            Cursor.lockState = CursorLockMode.Confined;
             Cursor.visible = true; 
+        } else {
+           Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false; 
         }
         
     }
